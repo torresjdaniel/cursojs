7152d1003eeeb6742961d7eba3c6c781770f6dc6
Dani Torres <danitorres16.93@gmail.com>
2021-09-28 20:58:24 -0300

    Incluyendo el script para el desafio de la clase 1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let nombre;
+let edad;
+let locacion;
+let anio;
+
+nombre = prompt("¡Buenas! ¿Cómo es tu nombre?");
+edad = prompt("Genial " + nombre + ", ¿Me podrías decir tu edad?");
+locacion = prompt("Entiendo " + nombre + "... y ... ¿Donde vivis?");
+anio = 2021 - parseInt(edad);
+
+alert(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
+" es un lindo lugar para vivir (o eso me contaron). Según mis calculos naciste en el año " + 
+anio + ". Sin más que decir, ¡Saludos! PD: Podes ver este mensaje cuando quieras, tocando la tecla F12 y luego hacer click en Console")
+
+console.log(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
+" es un lindo lugar para vivir (o eso me contaron). Según mis calculos naciste en el año " + 
+anio + ". Sin más que decir, ¡Saludos!");
