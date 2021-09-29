37be809323023179b3adc5aeafb3f38c6364f976
Dani Torres <danitorres16.93@gmail.com>
2021-09-28 21:05:08 -0300

    Arreglando el script

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ locacion = prompt("Entiendo " + nombre + "... y ... ¿Donde vivis?");
 anio = 2021 - parseInt(edad);
 
 alert(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
-" es un lindo lugar para vivir (o eso me contaron). Según mis calculos naciste en el año " + 
-anio + ". Sin más que decir, ¡Saludos! PD: Podes ver este mensaje cuando quieras, tocando la tecla F12 y luego hacer click en Console")
+" es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
+anio + ". Sin más que decir, ¡Saludos! PD: Si estas en una compu, podes ver este mensaje cuando quieras, tocando la tecla F12 y luego hacer click en Console")
 
 console.log(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
 " es un lindo lugar para vivir (o eso me contaron). Según mis calculos naciste en el año " + 
