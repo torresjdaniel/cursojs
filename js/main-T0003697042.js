060922afeb173ea47c5b5b0a68918063335aeac7
Dani Torres <danitorres16.93@gmail.com>
2021-09-30 23:52:58 -0300

    preparando segunda entrega

---
+++ js/main-T0003697042.js
@@ -0,0 +1,59 @@
+let nombre;
+let equipo;
+
+
+
+nombre = prompt("¡Buenas! ¿Cómo es tu nombre?");
+equipo = prompt("Genial " + nombre + ", ¿Con cuales de los 2 equipos de fútbol del súper clásico de Argentina te quedas?").toLowerCase();
+
+if ((equipo == "boca") && (equipo == "boca juniors") && (equipo == "river") && (equipo == "river plate")) {
+    if ((equipo=="boca") || (equipo == "boca juniors")){
+        alert(nombre +" elegiste la mitad más uno ;)");
+    }else{
+        alert(nombre + " elegiste al millonario ;)")
+    }
+} else {
+    alert(nombre + " mmm ese equipo no pertenece al super clásico :(")
+}
+
+
+
+
+
+// if (genero=="hombre") {
+//     alert("Tu nombre es: " + nombre + ". Tenes " + edad + ". Vivis en " + locacion +
+//  "PD: Si estas en una compu, podes ver este mensaje cuando quieras, tocando la tecla F12 y luego hacer click en Console");
+
+// console.log(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
+// " es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
+// anio + ". Sin más que decir, ¡Saludos!");  
+// }
+
+// if (genero==hombre) {
+//     alert(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
+// " es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
+// anio + ". Sin más que decir, ¡Saludos! PD: Si estas en una compu, podes ver este mensaje cuando quieras, tocando la tecla F12 y luego hacer click en Console");
+
+// console.log(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
+// " es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
+// anio + ". Sin más que decir, ¡Saludos!");  
+// }
+
+// if (genero==hombre) {
+//     alert(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
+// " es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
+// anio + ". Sin más que decir, ¡Saludos! PD: Si estas en una compu, podes ver este mensaje cuando quieras, tocando la tecla F12 y luego hacer click en Console");
+
+// console.log(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
+// " es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
+// anio + ". Sin más que decir, ¡Saludos!");  
+// }
+
+
+// alert(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
+// " es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
+// anio + ". Sin más que decir, ¡Saludos! PD: Si estas en una compu, podes ver este mensaje cuando quieras, tocando la tecla F12 y luego hacer click en Console");
+
+// console.log(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
+// " es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
+// anio + ". Sin más que decir, ¡Saludos!");
--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 let nombre;
-let edad;
-let locacion;
-let anio;
+let equipo;
+
+
 
 nombre = prompt("¡Buenas! ¿Cómo es tu nombre?");
-edad = prompt("Genial " + nombre + ", ¿Me podrías decir tu edad?");
-locacion = prompt("Entiendo " + nombre + "... y ... ¿Donde vivis?");
-anio = 2021 - parseInt(edad);
+equipo = prompt("Genial " + nombre + ", ¿Con cuales de los 2 equipos de fútbol del súper clásico de Argentina te quedas?").toLowerCase();
 
-alert(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
-" es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
-anio + ". Sin más que decir, ¡Saludos! PD: Si estas en una compu, podes ver este mensaje cuando quieras, tocando la tecla F12 y luego hacer click en Console");
+if ((equipo == "boca") || (equipo == "boca juniors") || (equipo == "river") || (equipo == "river plate")) {
+    if ((equipo=="boca") || (equipo == "boca juniors")){
+        alert(nombre +" elegiste la mitad más uno ;)");
+        console.log(nombre +" elegiste la mitad más uno ;)");
+    }else{
+        alert(nombre + " elegiste al millonario ;)")
+        console.log(nombre + " elegiste al millonario ;)");
+    }
+} else {
+    alert(nombre + " mmm ese equipo no pertenece al super clásico :(")
+    console.log(nombre + " mmm ese equipo no pertenece al super clásico :(");
+}
 
-console.log(nombre + " este es un simple código para pasar un desafio. Entiendo que " + locacion + 
-" es un lindo lugar para vivir (o eso me contaron). Según mis calculos (pueden fallar) naciste en el año " + 
-anio + ". Sin más que decir, ¡Saludos!");
