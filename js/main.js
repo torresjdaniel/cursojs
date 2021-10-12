231d54ca692a010a81ff9c70631c5a8defeadc22
Dani Torres <danitorres16.93@gmail.com>
2021-10-11 22:12:00 -0300

    Correción del desafio 4

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const pernil = 6500;
 let nombre;
 let opcion;
 let cantidad;
+let precioFinal;
 let opcion1 = "Opción 1: PALETA CHICA (Comen 10 personas) + 3 salsas y figazas. Costo: " + paletaChica + "$";
 let opcion2 = "Opción 2: PALETA CHICA (Comen 15 personas) + 3 salsas y figazas. Costo: " + paletaGrande + "$";
 let opcion3 = "Opción 3: PALETA CHICA (Comen 25 personas) + 5 salsas y figazas. Costo: " + pernil + "$";
@@ -21,8 +22,10 @@ cantidad = prompt("¿Qué cantidad queres de la opción elegida? (Ingresar solo 
 
 console.log("Pediste " + cantidad + " de esa opción");
 
-alert("El costo de tu pedido es: " + pedido(opcion, cantidad) + "$");
-console.log("El costo de tu pedido es: " + pedido(opcion, cantidad) + "$");
+precioFinal = pedido(opcion, cantidad)
+
+alert("El costo de tu pedido es: " + precioFinal + "$");
+console.log("El costo de tu pedido es: " + precioFinal + "$");
 
 function pedido(opcionElegida, cantidadElegida) {
     switch (opcionElegida) {
