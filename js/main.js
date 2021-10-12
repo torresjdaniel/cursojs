b8ee18434572c4b818db1bc6c1d0c76505ac7764
Dani Torres <danitorres16.93@gmail.com>
2021-10-11 22:09:39 -0300

    Cerrando el desafio complementario de la clase 4

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const pernil = 6500;
 let nombre;
 let opcion;
 let cantidad;
+let precioParcial;
+let precioFinal;
+let precioDescuento;
 let opcion1 = "Opción 1: PALETA CHICA (Comen 10 personas) + 3 salsas y figazas. Costo: " + paletaChica + "$";
 let opcion2 = "Opción 2: PALETA CHICA (Comen 15 personas) + 3 salsas y figazas. Costo: " + paletaGrande + "$";
 let opcion3 = "Opción 3: PALETA CHICA (Comen 25 personas) + 5 salsas y figazas. Costo: " + pernil + "$";
@@ -12,8 +15,8 @@ let opcion3 = "Opción 3: PALETA CHICA (Comen 25 personas) + 5 salsas y figazas.
 
 nombre = prompt("¡Hola!, ¿Cómo es tu nombre?");
 
-opcion = prompt(nombre + " te contamos acerca de nuestras opciones: \n" + opcion1 + "\n" + opcion2 + "\n" + opcion3 +
-                "\n¿Cúal te gustaría elegir? (Ingresar solo el número de la opción)");
+opcion = prompt(nombre + " te contamos que por esta semana, ¡Tenemos el 15% de descuento en el precio final de tu pedido! Estas son nuestras opciones: \n" 
+                + opcion1 + "\n" + opcion2 + "\n" + opcion3 + "\n¿Cúal te gustaría elegir? (Ingresar solo el número de la opción)");
                                 
 console.log("Elegiste la opción: " + opcion);
 
@@ -21,8 +24,22 @@ cantidad = prompt("¿Qué cantidad queres de la opción elegida? (Ingresar solo 
 
 console.log("Pediste " + cantidad + " de esa opción");
 
-alert("El costo de tu pedido es: " + pedido(opcion, cantidad) + "$");
-console.log("El costo de tu pedido es: " + pedido(opcion, cantidad) + "$");
+precioParcial = pedido(opcion, cantidad);
+
+console.log("Este es tu precio parcial: "+ precioParcial);
+
+const resta = (a,b) => a - b;
+const descuento = (d) => d * 0.15;
+
+precioDescuento = descuento(precioParcial);
+
+console.log("Este es tu descuento del 15%: "+ precioDescuento);
+
+
+precioFinal = resta(precioParcial,precioDescuento);
+
+alert("El costo de tu pedido es: " + precioFinal + "$");
+console.log("El costo de tu pedido es: " + precioFinal + "$");
 
 function pedido(opcionElegida, cantidadElegida) {
     switch (opcionElegida) {
