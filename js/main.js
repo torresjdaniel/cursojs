aa4d6ce7c063f669f01a32e72ddc6284f359f7fa
Dani Torres <danitorres16.93@gmail.com>
2021-10-14 23:59:52 -0300

    Terminando el desafio 6

--- js/main.js
+++ js/main.js
@@ -13,31 +13,34 @@ class Opcion{
     }
 }
 
-const opcion1 = new Opcion(1, "PALETA", "CHICA", 10, 4300, 3);
-const opcion2 = new Opcion(2, "PALETA", "GRANDE", 15, 4600, 3);
-const opcion3 = new Opcion(3, "PERNIL", "" , 25, 6500, 5);
+
+
+const opciones = [];
+
+opciones.push(new Opcion(1, "PALETA", "CHICA", 10, 4300, 3));
+opciones.push(new Opcion(2, "PALETA", "GRANDE", 15, 4600, 3));
+opciones.push(new Opcion(3, "PERNIL", "" , 25, 6500, 5));
 
 let nombre;
-let opcion;
+let seleccion;
 let cantidad;
 let precioFinal;
 
-console.log(opcion1.menu());
-console.log(opcion2.menu());
-console.log(opcion3.menu());
+for (const opcion of opciones )
+    console.log(opcion.menu())
 
 nombre = prompt("¡Hola!, ¿Cómo es tu nombre?");
 
-opcion = prompt(nombre + " te contamos acerca de nuestras opciones: \n" + opcion1.menu() + "\n" + opcion2.menu() + "\n" + opcion3.menu() +
+seleccion = prompt(nombre + " te contamos acerca de nuestras opciones: \n" + opciones[0].menu() + "\n" + opciones[1].menu() + "\n" + opciones[2].menu() +
                 "\n¿Cúal te gustaría elegir? (Ingresar solo el número de la opción)");
                                 
-console.log("Elegiste la opción: " + opcion);
+console.log("Elegiste la opción: " + seleccion);
 
 cantidad = prompt("¿Qué cantidad queres de la opción elegida? (Ingresar solo números)");
 
 console.log("Pediste " + cantidad + " de esa opción");
 
-precioFinal = pedido(opcion, cantidad)
+precioFinal = pedido(seleccion, cantidad)
 
 alert("El costo de tu pedido es: " + precioFinal + "$");
 console.log("El costo de tu pedido es: " + precioFinal + "$");
@@ -46,13 +49,13 @@ console.log("El costo de tu pedido es: " + precioFinal + "$");
 function pedido(opcionElegida, cantidadElegida) {
     switch (opcionElegida) {
         case "1":
-            return opcion1.precio * parseInt(cantidadElegida);
+            return opciones[0].precio * parseInt(cantidadElegida);
             break;
         case "2": 
-            return opcion2.precio * parseInt(cantidadElegida);
+            return opciones[1].precio * parseInt(cantidadElegida);
             break;
         case "3":
-            return opcion3.precio * parseInt(cantidadElegida);
+            return opciones[2].precio * parseInt(cantidadElegida);
             break;
         default:
             return 0;
